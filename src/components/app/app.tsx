851aa80131e57e0e49c80c0d144a4f3ad8403d7c
Anna <lilimp8@gmail.com>
2024-07-17 18:59:25 +0600

    Добавляет маршрутизацию

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,29 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login/login';
+import FavoritesPage from '../../pages/favorites/favorites';
+import OfferPage from '../../pages/offer/offer';
+import NotFoundPage from '../not-found-page/not-found-page';
+import PrivateRoute from '../private-route/private-route';
 import {OFFERS} from '../../data';
 
 function App(): JSX.Element {
   return (
-    <MainPage
-      offers={OFFERS}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Root} element={<MainPage offers={OFFERS} />} />
+        <Route path={AppRoute.Login} element={<LoginPage />} />
+        <Route path={AppRoute.Offer} element={<OfferPage />} />
+        <Route path={AppRoute.Favorites} element={
+          <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+            <FavoritesPage />
+          </PrivateRoute>
+        }
+        />
+        <Route path='*' element={<NotFoundPage />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
